df5ef8d597c97f8920df943fbc235f1c9df8cb1b
Захар <zakhar.backend@gmail.com>
2024-05-15 12:57:20 +0700

    feat(63-lab): сделал 63-ю лабораторную работу

---
+++ resources/js/63-lab/index.js
@@ -0,0 +1,16 @@
+var list = document.querySelector('.js-list');
+
+fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then(response => response.json())
+    .then(data => {
+        // Перебираем полученные данные
+        data.forEach(post => {
+            // Создаем элемент списка
+            var listItem = document.createElement('li');
+            // Задаем его содержимое
+            listItem.textContent = post.title;
+            // Добавляем его в список
+            list.appendChild(listItem);
+        });
+    })
+    .catch(error => console.error(error));
